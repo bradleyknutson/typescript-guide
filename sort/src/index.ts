b047fc6ba703e1ec7faa6aa59cf4f7f0a8319c0b
Bradley Knutson <bradley@bknutson.com>
2020-08-12 16:34:36 -0600

    Modify sort example to LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -7,18 +7,18 @@ import { LinkedList } from './LinkedList';
 // numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('XaayBcdE');
-charactersCollection.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('XaayBcdE');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-15);
-// linkedList.add(-3);
-// linkedList.add(4);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-15);
+linkedList.add(-3);
+linkedList.add(4);
 
-// linkedList.sort();
-// console.log(linkedList.convert());
+linkedList.sort();
+console.log(linkedList.convert());
 
 
 
